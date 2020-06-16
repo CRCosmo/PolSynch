28eb7b28e83ea71f82205ae6870595fcb604a48f
spinemart <29142329+spinemart@users.noreply.github.com>
2020-06-16 17:32:34 +0200

    updated version

--- genPolSynch.py
+++ genPolSynch.py
@@ -14,6 +14,7 @@ from scipy import stats
 import invRM as iRM 
 import genRMmap as gRM
 
+
 c = 299792458.0 #speed of light
 
 
@@ -41,7 +42,8 @@ if __name__ == '__main__':
     o.add_option('--stop',dest='stop',default=200,help='Stopping frequency in MHz')
     o.add_option('--nchan',dest='nchan',default=150,help='Number of frequency channels')
     o.add_option('--nside',dest='nside',default=128,help='Resolution of the map')
-    o.add_option('--filepath',dest='filepath',help='Path of Output map fits files')
+    o.add_option('--corr',dest='corr',action='store_true',help='Option for generating RM correlations')
+    o.add_option('--outdir',dest='outdir',help='Path of Output map fits files')
     o.add_option('--storeRM',dest='storeRM',action='store_true', help='Set option to store RM space Q,U maps')
     o.add_option('--readRM',dest='readRM',action='store_true', help='Set option to use RM maps from fits file')
     o.add_option('--fileq',dest='fileq',default='mapRM_Q',help='file RM Q maps from fits DEFAULT:mapRM_Q_nside[nside]_[RM].fits')
@@ -53,14 +55,16 @@ if __name__ == '__main__':
 
     nside = int(opts.nside)
     npix=hp.nside2npix(nside)
+    lmax=3*nside
     seed=int(opts.seed)
-    filepath=str(opts.filepath)
+    outdir=str(opts.outdir)
     start,stop,nchan = np.float(opts.start),np.float(opts.stop),np.float(opts.nchan)
     RM= np.load(args[0])['RM']     
     sigma = np.load(args[0])['sigma']  
     alpha = np.load(args[0])['alpha']  
+    rho_info=np.load(args[0])['rho_info']
     frequency = np.linspace(start,stop,nchan)
-    np.savez(open(filepath+'input_info.npz','wb'),frequency=frequency,nside=nside,seed=seed)
+    np.savez(open(outdir+'input_info.npz','wb'),frequency=frequency,nside=nside,seed=seed, corr=opts.corr)
     
     ##to convert in K
     convert=jyb2Tb(1,15.6,189, 4.3)  ##See Bernardi et al 2013
@@ -83,45 +87,43 @@ if __name__ == '__main__':
 
     else:
         print "Generating RM full sky maps.."
-        ##setting the seed
-        ## Here just used a simple generation for seed_vec 
-        seed_vec=np.arange(seed,seed+len(RM),1)
+
+        #create rho matrix nRMxlmax
+        rho=gRM.genRho(len(RM),lmax,rho_info, opts.corr)
+        #create cov matrix (lmax, nRM,nRM)
+        cov_vec=np.zeros((lmax,len(RM),len(RM)),dtype=float)
+        for l in range(1,lmax):
+            cov_vec[l,:,:]=gRM.RM_cov(len(RM), l, alpha, rho) 
+
+        #genarate the full RM cube
         RM_cube=np.zeros((len(RM),npix,2),dtype=float)
-
-        for rm in range(len(RM)):
-            map_QU=np.zeros((nside,2))
-            map_QU=gRM.genRMmapQU(alpha[rm], nside,seed_vec[rm],sigma[rm]) 
-            RM_cube[rm,:,:]=map_QU
-            print "Q,U maps genereted for RM=", RM[rm] 
-
-    	    if opts.storeRM: 
-
-                fname_Q=filepath+'mapRM_Q_nside'+str(nside)+'_seed'+str(seed)+'_'+str(rm)+'.fits'
-                hp.fitsfunc.write_map(fname_Q, map_QU[:,0])
-                fname_U=filepath+'mapRM_U_nside'+str(nside)+'_seed'+str(seed)+'_'+str(rm)+'.fits'
-                hp.fitsfunc.write_map(fname_U, map_QU[:,1])      
-                ## Note all file are stored with same seed value (the input one)
-
+        RM_cube=gRM.genRMcube_corr(len(RM), nside,seed, cov_vec,sigma)
+        if opts.storeRM: 
+            print "Storing RM maps"
+            for rm in range(len(RM)):
+                fname_Q=outdir+'mapRM_Q_nside'+str(nside)+'_seed'+str(seed)+'_'+str(rm)+'.fits'
+                hp.fitsfunc.write_map(fname_Q, RM_cube[rm,:,0])
+                fname_U=outdir+'mapRM_U_nside'+str(nside)+'_seed'+str(seed)+'_'+str(rm)+'.fits'
+                hp.fitsfunc.write_map(fname_U, RM_cube[rm,:,1])      
    
-            print "RM full sky maps generated."
+        print "RM full sky maps generated."
    
     
     print "Apply inverse RM"
     
  
     
-    for f in frequency:
-        p_map=iRM.P_l2(npix, RM_cube[:,:,0], RM_cube[:,:,1], RM, f)
-        print "Computed freq=",f
+    for ii,ff in enumerate(frequency):
+        p_map=iRM.P_l2(npix, RM_cube[:,:,0], RM_cube[:,:,1], RM, ff)
+        print "Computed freq=",ii,ff
 
    
         map_Q=p_map.real*convert
         map_U=p_map.imag*convert
+        
 
-        freq,df=str(f).split('.')
-
-        fname_Q=filepath+'map_Q_nside'+str(nside)+'_seed'+str(seed)+'_freq'+freq+'p'+df[:3]+'.fits'
+        fname_Q=outdir+'map_Q_nside'+str(nside)+'_seed'+str(seed)+'_freq%03d.fits'%(ii)
         hp.fitsfunc.write_map(fname_Q, map_Q)
-        fname_U=filepath+'map_U_nside'+str(nside)+'_seed'+str(seed)+'_freq'+freq+'p'+df[:3]+'.fits'
+        fname_U=outdir+'map_U_nside'+str(nside)+'_seed'+str(seed)+'_freq%03d.fits'%(ii)
         hp.fitsfunc.write_map(fname_U, map_U)
       
--- genRMmap.py
+++ genRMmap.py
@@ -4,43 +4,75 @@ import numpy as np
 import healpy as hp
 import random
 
-def genRMmapQU(alpha, nside,seed, sigma):
+#function to generate the NxNxlmax matrix to use for correlations
+def genRho(nRM,lmax,rho_info, corr):
+    rho=np.zeros((nRM,nRM,lmax))
+    if corr==True: 
+        print "Reading correlation info"
+        for rmi in range(nRM):
+            for rmj in range(nRM):
+                for l in range(lmax):
+                    if l<200: rho[rmi,rmj,l]=(rho_info[0]*l+rho_info[1])
+                    else: rho[rmi,rmj,l]=(rho_info[2]*l**rho_info[3])
+    return rho
+
+#function to create covariance matrix to use in RM map generation. If rho is zero no correlation are added.
+def RM_cov(nRM, ell, alpha, rho):
+    #nRM number N of RM values
+    #alpha is a N vector
+    #rho is a NxNxlmax matrix
+    cov=np.zeros((nRM,nRM),dtype=float)
+    for i in range(nRM):
+        for j in range(i,nRM):
+            if (j==i): cov[i,i]=ell**alpha[i]
+            else: 
+                cov[i,j]=0.75*np.sqrt(rho[i,j,ell])*np.sqrt(ell**alpha[i]*ell**alpha[j]) #rho is under squared root for Q,U to obtain rho in P
+                cov[j,i]=cov[i,j]
+    return cov  
+
+def genRMcube_corr(nRM, nside,seed, cov,sigma):
+    #sigma is N vector
+    #alpha is N vector
+    #cov is (lmax,nRM,nRM)
+    
     ell=np.arange(3*nside,dtype=float)
-    cl=np.zeros(len(ell))
-    beta=0.
-    lmin=2
-    for l in range(len(ell)):
-        if l<=lmin: cl[l]=0
-        else: cl[l]=10**beta*ell[l]**alpha
-            
+
         #Cl_Q,U created
     count=0
-    lmax=len(cl)
-    alm_Q_wo=np.zeros(lmax*(lmax+1)/2,dtype=complex)
-    alm_U_wo=np.zeros(lmax*(lmax+1)/2,dtype=complex)
+    lmax=3*nside
+    alm_Q_wo=np.zeros((lmax*(lmax+1)/2,nRM),dtype=complex)
+    alm_U_wo=np.zeros((lmax*(lmax+1)/2,nRM),dtype=complex)
 
     ##set seed 
     np.random.seed(seed)
     for l in range(lmax):
-        alm_Q_wo[count:count+l+1]=np.sqrt(cl[l])/np.sqrt(2)*np.random.normal(0,1,l+1)+1j*np.sqrt(cl[l])/np.sqrt(2)*np.random.normal(0,1,l+1)
-        alm_U_wo[count:count+l+1]=np.sqrt(cl[l])/np.sqrt(2)*np.random.normal(0,1,l+1)+1j*np.sqrt(cl[l])/np.sqrt(2)*np.random.normal(0,1,l+1)
-        count+=l+1
+            if l<=2: alm_Q_wo[count:count+l+1,:]=alm_U_wo[count:count+l+1,:]=0
+            else:
+                alm_Q_wo[count:count+l+1,:]=1./np.sqrt(2)*(np.random.multivariate_normal(np.zeros(nRM),cov[l,:],l+1)+1j*np.random.multivariate_normal(np.zeros(nRM),cov[l,:],l+1))
+                alm_U_wo[count:count+l+1,:]=1./np.sqrt(2)*(np.random.multivariate_normal(np.zeros(nRM),cov[l,:],l+1)+1j*np.random.multivariate_normal(np.zeros(nRM),cov[l,:],l+1))
+            count+=l+1
     
     #rearrange alm
-    alm_Q=np.zeros(lmax*(lmax+1)/2,dtype=complex)
-    alm_U=np.zeros(lmax*(lmax+1)/2,dtype=complex)
+    alm_Q=np.zeros((lmax*(lmax+1)/2,nRM),dtype=complex)
+    alm_U=np.zeros((lmax*(lmax+1)/2,nRM),dtype=complex)
     count=0
     for m in range(lmax):
         for l in range(m,lmax):
-            alm_Q[count]=alm_Q_wo[l*(l+1)/2+m]
-            alm_U[count]=alm_U_wo[l*(l+1)/2+m]
+            alm_Q[count,:]=alm_Q_wo[l*(l+1)/2+m,:]
+            alm_U[count,:]=alm_U_wo[l*(l+1)/2+m,:]
             count+=1
-    map_Q=hp.sphtfunc.alm2map(alm_Q, nside)
-    map_U=hp.sphtfunc.alm2map(alm_U, nside)
-    map_QU=np.zeros((len(map_Q),2))
-    ###note maps are resclaled
-    map_QU[:,0]=map_Q/np.std(map_Q)*sigma
-    map_QU[:,1]=map_U/np.std(map_U)*sigma
+    cubeQ=np.zeros((nRM,hp.nside2npix(nside)),dtype=float)
+    cubeU=np.zeros((nRM,hp.nside2npix(nside)),dtype=float)
+    cube_QU=np.zeros((nRM,hp.nside2npix(nside),2))
+    for rm in range(nRM):
+        alm_Q_C=alm_Q[:,rm]
+        alm_U_C=alm_U[:,rm]
+        cubeQ[rm,:]=hp.sphtfunc.alm2map(np.ascontiguousarray(alm_Q_C), nside)
+        cubeU[rm,:]=hp.sphtfunc.alm2map(np.ascontiguousarray(alm_U_C), nside)
+        
+        ###note maps are resclaled
+        cube_QU[rm,:,0]=cubeQ[rm,:]/np.std(cubeQ[rm,:])*sigma[rm]
+        cube_QU[rm,:,1]=cubeU[rm,:]/np.std(cubeU[rm,:])*sigma[rm]
     
     
-    return map_QU
+    return cube_QU
